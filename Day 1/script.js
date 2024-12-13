ea6341c4286f530d3f303e52e9cfbe39f49fe44b
Shubham <Shubham.glenhill@gmail.com>
2024-12-13 12:50:04 +0530

    new

---
+++ Day 1/script.js
@@ -0,0 +1,43 @@
+
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda"; 
+        
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name convention camelCase
+
+let PI = 3.14;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+
+let country = "India";
+let continent = "Asia";
+let population = "1.4 Billion";
+console.log(country);
+console.log(continent);
+console.log(population);
+
+*/
+
+
+let javaScriptIsFun = true;
+console.log(true);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javaScriptIsFun = "YES!";
+console.log(typeof javaScriptIsFun);
