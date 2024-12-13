bb0407824cc543a2dbba8958d03ab4b53c59c281
Shubham <Shubham.glenhill@gmail.com>
2024-12-14 05:16:06 +0530

    new

--- Day 1/script.js
+++ Day 1/script.js
@@ -1,3 +1,5 @@
+//Day 1
+
 /*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
@@ -57,6 +59,108 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+*/
+
+// Day 2
+
+/*
+let age = 30;
+age = 31; // mutated
+console.log(age);
+
+let year;
+year = 2004;//mutated
+
+const birthYear = 1997; 
+//birthYear = 1990; // cannot change
+console.log(birthYear);
+
+//const job; // cannot declare empty const
+
+var job = "programmer";
+job = "teacher";
+console.log(job);
+
+*/
+
+/*
+// Math Operators
+const now = 2037;
+
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 2, 2 ** 3);
+
+//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+
+//Assignment Operators
+let x = 10 + 5;
+x += 10; //x = x + 10 = 25
+x *= 4;// x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah);// >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+/*
+const now = 2037;
+
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+
+console.log(now - 1991 > now - 2018);
+
+//console.log(25 - 10 - 5);
+
+let x, y;
+
+x = y = 25 - 10 - 5;
+
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+*/
+
+/*
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear)+" years old "+job+"!";
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year-birthYear} year old ${job}`;
+console.log(jonasNew);
+
+console.log("String \n\
+multiple \n\
+lines");
+
+console.log(`String with
+multiple
+lines`);
 
 */
 
